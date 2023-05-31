b066e146e100385b5c58af00ab7267ccd5790a61
ranesayalee <135012911+ranesayalee@users.noreply.github.com>
2023-05-31 10:57:25 +0800

    Update streamlit_app.py
    Display my fruit liabrary

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 
 import streamlit
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
 streamlit.title('My parents new healthy dinner contains fruits, oats, pulses, vegetables')
 streamlit.text('They should eat balanced food')
 streamlit.header('Breakfast Menu')
