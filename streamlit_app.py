cc3f9a30921c32b912a46fee5b11dbfb9e0cb3b8
ranesayalee <135012911+ranesayalee@users.noreply.github.com>
2023-06-08 15:53:21 +0800

    Update streamlit_app.py
    New response changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,7 @@ streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
 streamlit.text(' 🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
