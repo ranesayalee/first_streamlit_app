59f409b88f1a124920b7aa2dab3fc378ac4143cc
ranesayalee <135012911+ranesayalee@users.noreply.github.com>
2023-06-20 10:23:51 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 
 import streamlit
 import pandas
+import requests
 import snowflake.connector
+from urllib.error import URLError
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -25,10 +28,12 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + fruit_choice)
 
-import snowflake.connector
+streamlit.stop()
+
+
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #streamlit.text(fruityvice_response.json())
 
