2759d413029010d9157e8f356c69dcd088afb843
ranesayalee <135012911+ranesayalee@users.noreply.github.com>
2023-06-20 10:53:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,20 +24,19 @@ streamlit.text(' 🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + this_fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized
-  
-streamlit.header("Fruityvice Fruit Advice!")
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-      streamlit.error("Please select a fruit to get information.")
-  else: 
-      back_from_function = get_fruityvice_data(fruit_choice)
-      streamlit.dataframe(back_from_function)
-except URLError as e :
+def get_fruityvice_data(this_fruit_choice): 
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice) 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
+return fruityvice_normalized
+
+streamlit.header("Fruityvice Fruit Advice!") 
+try: 
+  fruit_choice = streamlit.text_input('What fruit would you like information about?') 
+  if not fruit_choice: streamlit.error("Please select a fruit to get information.") 
+else: back_from_function = get_fruityvice_data(fruit_choice) 
+  streamlit.dataframe(back_from_fuction)
+
+except URLError as e:
   streamlit.error()
 
 streamlit.stop()
